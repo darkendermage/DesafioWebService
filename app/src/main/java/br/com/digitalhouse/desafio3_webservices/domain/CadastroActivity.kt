ee2b95625816af04918d2e85c27fd6e76742ae7c
Leonardo Carvalho <leo.carvalho100@gmail.com>
2020-11-30 22:37:55 -0300

    Login

---
+++ app/src/main/java/br/com/digitalhouse/desafio3_webservices/domain/CadastroActivity.kt
@@ -0,0 +1,12 @@
+package br.com.digitalhouse.desafio3_webservices.domain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.digitalhouse.desafio3_webservices.R
+
+class CadastroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+    }
+}
---
+++ app/src/main/java/br/com/digitalhouse/desafio3_webservices/model/HQ.kt
@@ -0,0 +1,9 @@
+package br.com.digitalhouse.desafio3_webservices.model
+
+data class HQ(
+    val title: String,
+    val description: String,
+    val thumbnail: String,
+    val price: Int,
+    val pages: Int
+)
